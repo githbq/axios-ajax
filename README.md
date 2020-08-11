199e27d670aac52953acad914d49c99337629da1
hubiqing <hbqhbq123@qq.com>
2020-08-11 18:25:57 +0800

    axios-ajax

--- README.md
+++ README.md
@@ -1,18 +1,35 @@
-# 项目描述
-
-![my love](./logo.png) 
+# axios-ajax
 
 ## 功能介绍
-1. 简介1 
-2. 简介2   
+1. 针对axios使用二次封装，更友好的发请求 
+2. 对外输出 axiosInstance 与 restful 对象, axios 为新实例对象  
 
 ## 安装
 ```
-yarn
+npm install axios-ajax
 ```
 
 ## 测试   
+```js
+import {axiosInstance,restful} from 'axios-ajax' 
+// 第二个参数为原生axios options
+const response = await restful.get('http://www.com/api',{a:1,b:2})
+const response = await restful.post('http://www.com/api',{a:1,b:2})
+const response = await restful.put('http://www.com/api',{a:1,b:2})
+const response = await restful.delete('http://www.com/api',{a:1,b:2})
+const response = await restful.patch('http://www.com/api',{a:1,b:2})
+
+// axiosInstance 为axios实例对象，
+// 你可以使用 axiosInstance.interceptors.request.use 或者 axiosInstance.interceptors.response.use 
+
 ```
-npm run test    
-```
- 
+## restful 动词介绍
+ ```
+// GET（SELECT）：从服务器取出资源（一项或多项）。
+// POST（CREATE）：在服务器新建一个资源。
+// PUT（UPDATE）：在服务器更新资源（客户端提供改变后的完整资源）。
+// PATCH（UPDATE）：在服务器更新资源（客户端提供改变的属性）。
+// DELETE（DELETE）：从服务器删除资源。
+ ```
+
+相关 restful api 介绍链接:<http://www.ruanyifeng.com/blog/2014/05/restful_api.html>
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,71 @@
-/**
- * entry
- */
-console.log('hello typescript') 
-debugger 
+import axios from 'axios'
+
+// GET（SELECT）：从服务器取出资源（一项或多项）。
+// POST（CREATE）：在服务器新建一个资源。
+// PUT（UPDATE）：在服务器更新资源（客户端提供改变后的完整资源）。
+// PATCH（UPDATE）：在服务器更新资源（客户端提供改变的属性）。
+// DELETE（DELETE）：从服务器删除资源。 
+const defaultOptions = {
+    method: 'get', // method:  get post delete put patch
+    withCredentials: true, // 设置该属性可以把 cookie 信息传到后台 
+    headers: {
+        // headers 的键是不区分大小写的
+        Accept: 'application/json',
+        'Content-Type': 'application/json; charset=utf-8'
+    },
+}
+const axiosInstance = axios.create({
+    timeout: 15000, // 请求超时时间
+})
+
+
+function callApi({
+    url,
+    data = {},
+    options = {},
+    prefix = 'api',
+}) {
+    if (!url) {
+        const error = new Error('请传入 url')
+        return Promise.reject(error)
+    }
+
+    let fullUrl = ''
+    if (url.indexOf('http') === 0) {
+        fullUrl = url // 如果是绝对路径，则直接发请求
+    } else {
+        url.replace(/^\//, '')
+        fullUrl = `${prefix}${url}`
+    }
+    const newOptions = { ...defaultOptions, ...options }
+
+    return (axiosInstance as any)({
+        url: fullUrl,
+        ...newOptions,
+    })
+}
+const restful = {
+    get(url, params, options) {
+        return callApi({
+            url, options: { params, method: 'get', ...options },
+        })
+    },
+
+    post(url, data, options) {
+        return callApi({ url, data, options: { method: 'post', ...options } })
+    },
+    put(url, data, options) {
+        return callApi({ url, data, options: { method: 'put', ...options } })
+    },
+    delete(url, data, options) {
+        return callApi({ url, data, options: { method: 'delete', ...options } })
+    },
+    patch(url, data, options) {
+        return callApi({ url, data, options: { method: 'patch', ...options } })
+    },
+}
+
+export {
+    axiosInstance,
+    restful
+} 
